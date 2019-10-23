a96ac1db269eadb56c7eaddea1a9b7d9b8464340
Daniel Leal Perez <dannniel-cast@hotmail.com>
2019-10-23 13:53:41 +0200

    Initial Commit

---
+++ src/shared/constant.ts
@@ -0,0 +1,2 @@
+export let gameRegions: string[] = ['PAL', 'NTSC-J', 'NTSC-U', 'NTSC-A'];
+export let platforms: string[] = ['Todas', 'Nintendo DS', 'Nintendo 3DS', 'Nintendo Wii', 'Nintendo WiiU', 'Playstation', 'Playstation 2', 'Playsation 3', 'Playstation 4', 'Xbox 360'];
